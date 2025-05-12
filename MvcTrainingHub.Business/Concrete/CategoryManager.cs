5cec8272c5482b777549247a8a3258ca6b6b8f10
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-12 15:56:51 +0200

    feat(business): add basic CategoryManager with validation and direct repository usage

---
+++ MvcTrainingHub.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using MvcTrainingHub.DataAccess.Concrete.Repositories;
+using MvcTrainingHub.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcTrainingHub.Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+                // error message
+            }
+            else
+            {
+                repository.Insert(p);
+            }
+        }
+    }
+}
