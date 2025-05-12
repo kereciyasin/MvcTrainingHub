9d2ae3440bef9a758e89cd8bfe3029dfd5218977
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-12 18:45:43 +0200

    category update

--- MvcTrainingHub.Business/Concrete/CategoryManager.cs
+++ MvcTrainingHub.Business/Concrete/CategoryManager.cs
@@ -20,15 +20,15 @@ namespace MvcTrainingHub.Business.Concrete
 
         public void CategoryAddBL(Category p)
         {
-            repository.Insert(p);
-            //if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
-            //{
-            //    // error message
-            //}
-            //else
-            //{
-            //    repository.Insert(p);
-            //}
+
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+                // error message
+            }
+            else
+            {
+                repository.Insert(p);
+            }
         }
     }
 }
