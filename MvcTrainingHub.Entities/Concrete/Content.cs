fb5cdecad556855dee4ca15a31c42134ac281afa
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-12 12:36:16 +0200

    key added

--- MvcTrainingHub.Entities/Concrete/Content.cs
+++ MvcTrainingHub.Entities/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace MvcTrainingHub.Entities.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentID { get; set; }
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
--- MvcTrainingHub.Entities/Concrete/Heading.cs
+++ MvcTrainingHub.Entities/Concrete/Heading.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace MvcTrainingHub.Entities.Concrete
 {
     public class Heading
     {
+        [Key]
         public int HeadingID { get; set; }
         public string HeadingName { get; set; }
         public DateTime HeadingDate { get; set; }
